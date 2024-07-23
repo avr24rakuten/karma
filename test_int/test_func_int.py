d4eedfb410f03853c83e8bce50dd550507277666
Arnaud <arnaud.kreweras@gmail.com>
2024-07-23 22:23:16 +0200

    host_IP handling for testing APIs

--- test_int/test_func_int.py
+++ test_int/test_func_int.py
@@ -1,14 +1,22 @@
 import pytest
+import os
+import sys
 import requests
 
 # from lib.mysql import *
+
+try:
+    host_ip = os.environ.get('HOST_IP')
+except:
+    print("HOST IP not in HOST_IP Env variable") 
+    sys.exit(1)
 
 #############################################
 # STATUS & HEALTHCHECK
 #############################################
 
 def test_get_karma_healthcheck():
-    response = requests.get('http://172.26.107.175:8000/karma_healthcheck')
+    response = requests.get('http://{}:8000/karma_healthcheck'.format(host_ip))
     assert response.status_code == 200
 
 #############################################
