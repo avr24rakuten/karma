0c9daab0eb974120a12c05031ff0881746091616
Arnaud <arnaud.kreweras@gmail.com>
2024-08-03 20:01:18 +0200

    To docker compose v2

--- setup_int.sh
+++ setup_int.sh
@@ -131,4 +131,4 @@ done
 # LAUNCH DOCKER COMPOSE
 echo "...:: Docker compose start..."
 pwd
-docker-compose -f docker/docker-compose-karma-int.yml up -d
+docker compose -f docker/docker-compose-karma-int.yml up -d
