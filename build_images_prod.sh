0e8ca86b64191549b23a42d4fa9074f7b7af820f
Arnaud <arnaud.kreweras@gmail.com>
2024-09-02 16:44:15 +0200

    test for push image from main

--- build_images_prod.sh
+++ build_images_prod.sh
@@ -67,12 +67,11 @@ for KARMA_IMAGE in "${KARMA_IMAGES[@]}"; do
                 BUILD_CMD+=" --build-arg $ARG=${!VAR}"
             fi
         done
-        echo $RELEASE_TAG
         TAG=${RELEASE_TAG:-latest}
         echo "RELEASE TAG PLACE"
         echo $TAG
-        # BUILD_CMD+=" -f docker/${KARMA_IMAGE}/Dockerfile.prod -t avr24rakuten/${KARMA_IMAGE}:${TAG} ."
-        BUILD_CMD+=" -f docker/${KARMA_IMAGE}/Dockerfile.prod -t avr24rakuten/${KARMA_IMAGE}:latest ."
+        BUILD_CMD+=" -f docker/${KARMA_IMAGE}/Dockerfile.prod -t ${KARMA_IMAGE}:${TAG} ."
         eval $BUILD_CMD || { echo "Image creation failed"; exit 1; }
+
     fi
 done
